a17d28f1c9dd3007764c06cdb65777560a8ede9b
nbicode <nbi.decode@gmail.com>
2016-04-12 04:22:28 +0000

    exercise 12 & 13 added

---
+++ exercise12.sql
@@ -0,0 +1,10 @@
+﻿
+select name from (select books.title AS name, 
+count(distinct editions.type) as count
+from
+books, editions
+where books.id = editions.book_id
+group by books.title) AS temp
+where temp.count = 2
+	
+
---
+++ exercise13.sql
@@ -0,0 +1,13 @@
+﻿
+select publishers.name,
+count(editions.edition) AS "Number of Editions",
+AVG(stock.retail) AS "Average Book Sale Price"
+from publishers, editions, stock
+where 
+publishers.id = editions.publisher_id
+and
+editions.isbn = stock.isbn
+group by
+publishers.name
+	
+
